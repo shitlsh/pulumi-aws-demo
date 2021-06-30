40c4d20e0e5267a8da64461585a6bcb34f9775fb
Tailong <shitlsh@126.com>
2021-06-30 16:28:27 +0800

    add sns & schedulRule to stack

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/cloudwatch"
+	sns2 "github.com/pulumi/pulumi-aws/sdk/v4/go/aws/sns"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"os"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		//create a event rule triggers sns topic every 5 minutes
+		scheduleRule, err := cloudwatch.NewEventRule(ctx, "pulumi-aws-demo-schedule-rule", &cloudwatch.EventRuleArgs{
+			Description:  pulumi.String("Trigger pulumi-aws-demo-main-sns every 5 minutes"),
+			ScheduleExpression:  pulumi.String("rate(5 minutes)"),
+		})
 		if err != nil {
 			return err
 		}
-
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		// Create an AWS resource (SNS:Topic)
+		mainSns, err := sns2.NewTopic(ctx,"pulumi-aws-demo-main-sns",nil)
+		if err != nil {
+			return err
+		}
+		//Link event rule to trigger sns
+		_, err = cloudwatch.NewEventTarget(ctx, "sns", &cloudwatch.EventTargetArgs{
+			Rule: scheduleRule.Name,
+			Arn:  mainSns.Arn,
+		})
+		if err != nil {
+			return err
+		}
+		//create subscriptions for mainSns
+		_, err = sns2.NewTopicSubscription(ctx,"pulumi-aws-demo-main-sns-email-sub", &sns2.TopicSubscriptionArgs{
+			Topic: mainSns,
+			Endpoint: pulumi.String(os.Getenv("MY_EMAIL_ADDRESS")),
+			Protocol: pulumi.String("email"),
+		})
 		return nil
 	})
 }
+
