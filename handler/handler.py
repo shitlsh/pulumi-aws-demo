81876b1aea55c15ac2e2ebced50e904345d6ed74
Tailong <shitlsh@126.com>
2021-07-06 15:47:16 +0800

    fix set image env

--- handler/handler.py
+++ handler/handler.py
@@ -5,6 +5,6 @@ import random
 def handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
-    a = random.randint(0, 25)
+    a = random.randint(0, 28)
     if a == 0:
         raise RuntimeError("Test dead letter queue error")
